e9a3b607da47e48b612ad7c2f0dfe9e3ccb70e15
wahyu agus arifin <itpohgero@gmail.com>
2025-02-14 14:09:41 +0700

    user

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-// src/main.rs
 pub mod interface;
 
 use axum::{
     routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    Router,
 };
-use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
@@ -17,31 +14,12 @@ async fn main() {
     let app = Router::new()
         .route("/", get(interface::controllers::hello_controller::hello))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route(
+            "/users",
+            post(interface::controllers::user_controller::create_user),
+        );
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn create_user(
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    (StatusCode::CREATED, Json(user))
-}
-
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
 }
