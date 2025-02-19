02bc3fa226c2eb0ddf9ecee81eb85e452ee57040
wahyu agus arifin <itpohgero@gmail.com>
2025-02-19 10:22:20 +0700

    delivery

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod domain;
-pub mod interface;
+pub mod delivery;
 
 use axum::{
     routing::{get, post, put, delete},
@@ -11,12 +11,12 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/", get(interface::controllers::hello_controller::hello))
-        .route("/users", post(interface::controllers::user_controller::create_user))
-        .route("/users", get(interface::controllers::user_controller::list_users))
-        .route("/users/{id}", get(interface::controllers::user_controller::get_user))
-        .route("/users/{id}", put(interface::controllers::user_controller::update_user))
-        .route("/users/{id}", delete(interface::controllers::user_controller::delete_user));
+        .route("/", get(delivery::controllers::hello_controller::hello))
+        .route("/users", post(delivery::controllers::user_controller::create_user))
+        .route("/users", get(delivery::controllers::user_controller::list_users))
+        .route("/users/{id}", get(delivery::controllers::user_controller::get_user))
+        .route("/users/{id}", put(delivery::controllers::user_controller::update_user))
+        .route("/users/{id}", delete(delivery::controllers::user_controller::delete_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
