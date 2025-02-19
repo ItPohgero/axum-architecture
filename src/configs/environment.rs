4310bb062250bec3d5c10be78398e4e58460500e
wahyu agus arifin <itpohgero@gmail.com>
2025-02-19 11:55:24 +0700

    environment

---
+++ src/configs/environment.rs
@@ -0,0 +1,26 @@
+use config::{Config, File, FileFormat};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub name: String,
+    pub port: u32,
+    pub database: DatabaseConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DatabaseConfig {
+    pub host: String,
+    pub port: u32,
+    pub name: String,
+    pub username: String,
+    pub password: String,
+}
+
+pub fn main() -> AppConfig {
+    let config = Config::builder()
+        .add_source(File::new("env.yaml", FileFormat::Yaml))
+        .build().unwrap();
+
+    config.try_deserialize().unwrap()
+}
